5e19e750d871c476dc2e84acb4cd457f66b4cc71
David Street <street.davide@gmail.com>
2018-01-12 21:16:44 -0500

    Add service class helper

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ module.exports = {
 	App: require('./lib/app'),
 	Client: require('./lib/client'),
 	ModelBuilder: require('./lib/model-builder'),
-	ModelService: require('./lib/model-service')
+	ModelService: require('./lib/model-service'),
+	RegisterServiceClass: require('./lib/helpers/register-service-class')
 }
